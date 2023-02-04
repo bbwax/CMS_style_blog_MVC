cdcc9f8c18ded62f6347b4f0bb0fc1a1da0eec59
bwax <braxton.waxdeck@gmail.com>
2023-02-04 15:36:03 -0800

    created user model, working on setting cookie

--- controllers/apis/users.js
+++ controllers/apis/users.js
@@ -1,11 +1,59 @@
 const { Router } = require('express');
 
+const User = require('../../models/User');
+
 const usersRouter = new Router();
 
-usersRouter.post("/login", (req, res) => {
-    console.log(req.body);
+usersRouter.post("/login", async (req, res) => {
+    const { username, password } = req.body;
+
+    const user = await User.findOne({
+        where: {
+            username,
+        }
+    });
+
+    if (!user) {
+        res.status(401).end('User not found');
+        return;
+    }
+
+    if (user.password !== password) {
+        res.status(401).end('Password did not match');
+        return;
+    }
+
+    // const token = jwt.sign({ id: user.id }, process.env.JWT_KEY);
+
+    // res.cookie('logintoken', token, { httpOnly: true });
+
 
     res.end();
 });
+
+usersRouter.post('/', async (req, res) => {
+    const { username, password } = req.body;
+
+    const user = await User.findOne({
+        where: {
+            username,
+        }
+    });
+
+    if (user) {
+        res.status(409).end('User already exists');
+        return;
+    }
+
+    const newUserObject = await User.create({
+        username,
+        password,
+    });
+
+    res.status(200).json({
+        id: newUserObject.id,
+
+    });
+})
 
 module.exports = usersRouter;
