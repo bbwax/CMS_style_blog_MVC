ab74f5330a6ba16e74e1e1212ee2a878129fe63e
bwax <braxton.waxdeck@gmail.com>
2023-02-05 22:18:47 -0800

    fixing config for heroku

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,39 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize(
+// const sequelize = new Sequelize(
+//     process.env.DB_DB,
+//     process.env.DB_USER,
+//     process.env.DB_PASS,
+//     {
+//         host: process.env.DB_HOST,
+//         dialect: 'mysql'
+//     },
+// );
+let sequelize;
+if (process.env.DATABASE_URL) {
+  console.log("SETTING DB USING URL", process.env.DATABASE_URL);
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
+  );
+} else {
+  sequelize = new Sequelize(
     process.env.DB_DB,
     process.env.DB_USER,
     process.env.DB_PASS,
     {
-        host: process.env.DB_HOST,
-        dialect: 'mysql'
-    },
-);
+      host: process.env.DB_HOST,
+      dialect: 'mysql',
+      dialectOptions: {
+        decimalNumbers: true,
+      },
+    }
+  );
+}
 
 module.exports = sequelize;
